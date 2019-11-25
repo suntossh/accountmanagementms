731e3264dcedc6cb8f9b39fea187e4c59075b6ac
Suntossh <suntossh.numerouno@gmail.com>
2019-11-25 06:38:54 -0500

    Simple Account management Microservice which is a Eureka Client and registers itself to Eureka Discovery Server

---
+++ src/main/java/com/suntossh/springboot/client/accountmanagement/AccountmanagementmsApplication.java
@@ -0,0 +1,13 @@
+package com.suntossh.springboot.client.accountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmanagementmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmanagementmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suntossh/springboot/client/accountmanagement/AccountmanagementmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suntossh.springboot.client.accountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmanagementmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
